1087ac6132c10906e993bb725ec765cceca3bfdb
MMMMunshif <munshif20032003@gmail.com>
2026-08-16 16:41:14 +0530

    Add RBAC with Super Admin protected route

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { RolesGuard } from '../auth/guards/roles.guard';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, RolesGuard],
 })
 export class UsersModule {}
